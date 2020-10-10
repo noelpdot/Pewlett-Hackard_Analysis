5d1d94dac9d00a218b970956b00fa7b5d6384d7f
noelpdot <66786205+noelpdot@users.noreply.github.com>
2020-10-09 20:05:09 -0400

    ERD code final for tables.

--- schema.sql
+++ schema.sql
@@ -1,19 +1,19 @@
 --Creating employee table for PH-EmpDB
 CREATE TABLE employees (
-emp_no INT NOT NULL,
-birth_date DATE NOT NULL,
-first_name VARCHAR NOT NULL,
-last_name VARCHAR NOT NULL,
-gender VARCHAR(1) NOT NULL,
-hire_date DATE NOT NULL,
-PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
 SELECT * FROM employees;
 
 --Creating dept manager table 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT  NULL,
+	dept_no VARCHAR(4) NOT  NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
@@ -26,37 +26,37 @@ SELECT * FROM dept_manager;
 
 --Creating salaries table
 CREATE TABLE salaries(
-emp_no INT NOT NULL,
-salary INT NOT NULL,
-from_date DATE NOT NULL, 
-to_date DATE NOT NULL, 
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL, 
+	to_date DATE NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
 );
 
 SELECT *FROM salaries;
 
 --Creating dept. employee table
 CREATE TABLE dept_employees(
-emp_no INT NOT NULL,
-dept_no VARCHAR(4) NOT NULL,
-from_date DATE NOT NULL, 
-to_date DATE NOT NULL, 
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (emp_no, dept_no)
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL, 
+	to_date DATE NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT *FROM salaries;
 
 --Creating Titles table
 CREATE TABLE titles (
-emp_no INT NOT NULL,
-title VARCHAR (40) NOT NULL,
-from_date DATE NOT NULL, 
-to_date DATE NOT NULL, 
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no, from_date)
+	emp_no INT NOT NULL,
+	title VARCHAR (40) NOT NULL,
+	from_date DATE NOT NULL, 
+	to_date DATE NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
 );
 
 SELECT *FROM titles;
